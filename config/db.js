292a41af340ca2764df42084c52313b1cce077f6
Your Name <you@example.com>
2026-04-24 21:54:57 +0300

    use MONGODB_URI from environment

--- config/db.js
+++ config/db.js
@@ -1,16 +1,13 @@
 // config/db.js
-let express = require("express");
-let mongoose = require("mongoose");
-// Connection and port and listen
+const mongoose = require("mongoose");
+
 async function connectTOMongoDB() {
-  mongoose
-    .connect("mongodb://127.0.0.1:27017/rental_tracker")
-    .then(() => {
-      console.log("Successfully connected to mongodb ✅");
-    })
-    .catch((error) => {
-      console.log("Error connecting to mongoDB ❌: ", error);
-    });
+  // Use the environment variable if available, otherwise fall back to local
+  const uri =
+    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/rental_tracker";
+
+  await mongoose.connect(uri);
+  console.log(`✅ Successfully connected to MongoDB`);
 }
 
 module.exports = connectTOMongoDB;
